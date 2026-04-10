767fedeaa82dc2c9ebfcbb128cb288d8962d54fc
Cadu <ceduardovidalf@gmail.com>
2026-04-09 21:31:28 -0300

    Implementing Order Aggregate Root

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/OrdemItemId.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.order.service.domain.valueobject;
+
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+public class OrdemItemId extends BaseId<Long> {
+    protected OrdemItemId(Long value) {
+        super(value);
+    }
+}
